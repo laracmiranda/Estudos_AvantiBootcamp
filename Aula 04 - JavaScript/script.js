2cfdf8875e9b4e663c4453e2962dd4643eb2204f
Lara <laracorsinidemiranda@gmail.com>
2025-06-23 12:14:12 -0300

    Aula 04 sobre Javascript

---
+++ Aula 04 - JavaScript/script.js
@@ -0,0 +1,9 @@
+// Repetição
+for (let i=0;i>=5;i++){
+    console.log('Executando ',i)
+}
+
+// Arrays
+let animais = ['leão', 'gato', 'cachorro']
+animais.push('papagaio')
+console.log('Animal ', animais[3])
