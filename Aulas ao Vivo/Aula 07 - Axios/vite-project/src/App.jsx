3a755bea8c9f137204e1cb01b7bdf973ec3aab01
Lara Corsini <laracorsinidemiranda@gmail.com>
2025-08-10 18:43:57 -0300

    feat: Adicionando proteção de rotas

--- Aulas ao Vivo/Aula 07 - Axios/vite-project/src/App.jsx
+++ Aulas ao Vivo/Aula 07 - Axios/vite-project/src/App.jsx
@@ -1,20 +1,29 @@
 
 import { UserForm } from "./components/UserForm"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { LandingPage } from "./pages/LandingPage"
 import { Home } from "./pages/Home"
 import { UserFormPage } from "./pages/UserFormPage"
 import { Login } from "./pages/Login"
+import { useAuth } from "./context/AuthContext"
+
+// Criando proteção para as rotas
+const AuthRoutes = ({component: Component}) => {
+  const { userId } = useAuth();
+  // UserID - Se existir: Renderiza o componente. Se não existir: Direciona pra página inicial
+  return userId ? <Component /> : <Navigate to="/login"/>
+}
+
 function App() {
 
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/usuarios" element={<Home />} />
-          <Route path="/registro" element={<UserFormPage />} />
-          <Route path="/editar-usuario/:id" element={<UserFormPage />} />
+          <Route path="/" element={<AuthRoutes component={LandingPage} />} />
+          <Route path="/usuarios" element={<AuthRoutes component={Home} />} />
+          <Route path="/registro" element={<AuthRoutes component={UserFormPage} />} />
+          <Route path="/editar-usuario/:id" element={<AuthRoutes component={UserFormPage} />} />
         </Routes>
       </BrowserRouter>
   )
